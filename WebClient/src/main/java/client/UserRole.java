8e6a32f01fdc6935b9b7f45cf83cc4cf2e6c6ba4
fenrir4ik <emil.nadzhiev@gmail.com>
2021-12-13 19:46:10 +0200

    Java EE project setup

---
+++ WebClient/src/main/java/client/UserRole.java
@@ -0,0 +1,51 @@
+
+package client;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for User_role.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="User_role"&gt;
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *     &lt;enumeration value="Admin"/&gt;
+ *     &lt;enumeration value="User"/&gt;
+ *   &lt;/restriction&gt;
+ * &lt;/simpleType&gt;
+ * </pre>
+ * 
+ */
+@XmlType(name = "User_role", namespace = "http://rtx_computershop.ua/entity/orders")
+@XmlEnum
+public enum UserRole {
+
+    @XmlEnumValue("Admin")
+    ADMIN("Admin"),
+    @XmlEnumValue("User")
+    USER("User");
+    private final String value;
+
+    UserRole(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static UserRole fromValue(String v) {
+        for (UserRole c: UserRole.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
